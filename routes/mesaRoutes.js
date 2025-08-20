166d1f31f8e071db2da3cda2330d868156b568e5
mezquitaboliranabar-creator <mezquitaboliranabar@gmail.com>
2025-08-19 23:43:32 -0500

    Modificacion clase mesaController

--- routes/mesaRoutes.js
+++ routes/mesaRoutes.js
@@ -1,8 +1,18 @@
+// routes/mesaRoutes.js
 const express = require("express");
-const router = express.Router();
-const { crearMesa, obtenerMesas } = require("../controllers/mesaController");
+const {
+  crearMesa,
+  obtenerMesas,
+  obtenerMesaPorId,
+  // obtenerMesaPorNumero, // (opcional)
+} = require("../controllers/mesaController");
 
+const router = express.Router();
+
+// OJO: si algún día usas /by-numero/:numero, ponlo ANTES de "/:id"
 router.post("/", crearMesa);
 router.get("/", obtenerMesas);
+// router.get("/by-numero/:numero", obtenerMesaPorNumero); // opcional
+router.get("/:id", obtenerMesaPorId); // <- ESTA ES LA QUE FALTA
 
 module.exports = router;
